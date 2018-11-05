4ce5982c7653d284b732584def3a902af7b17fbf
Tuan Anh <tuananhvp25895@gmail.com>
2018-11-05 22:20:21 +0700

    Add account activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
-  before_save {email.downcase!}
+  attr_accessor :remember_token, :activation_token
+  before_save :downcase_email
+  before_create :create_activation_digest
   validates :name, presence: true, length: {maximum: Settings.User.number}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: Settings.User.number1},
@@ -25,6 +26,12 @@ class User < ApplicationRecord
       return false if remember_digest.nil?
       BCrypt::Password.new(remember_digest).is_password?remember_token
     end
+
+    def authenticated? attribute, token
+      digest = send "#{attribute}_digest"
+      return false if digest.nil?
+      BCrypt::Password.new(digest).is_password? token
+    end
   end
 
   def remember
@@ -34,5 +41,25 @@ class User < ApplicationRecord
 
   def forget
     update remember_digest: nil
+  end
+
+  def activate
+    update activated: true
+    update activated_at: Time.zone.now
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
+  private
+
+  def downcase_email
+    self.email.downcase!
+  end
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest activation_token
   end
 end
